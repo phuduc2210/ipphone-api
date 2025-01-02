c723289e3ecbda395a857846329804bf1b64ddfe
ducnp2 <phuduc221@gmail.com>
2025-01-02 14:30:36 +0700

    config database

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -3,9 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection({
+// export const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT_DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit:0,
+  connectTimeout: 10000, 
+  acquireTimeout: 10000,
 });
