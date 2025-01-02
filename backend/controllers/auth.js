ff55b428e89f3d66f8f2479506fc75904710059c
ducnp2 <phuduc221@gmail.com>
2025-01-02 15:10:11 +0700

    fix store otp in database

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,7 +6,7 @@ export const register = (req, res) => {
   //CHECK EXISTING USER
   const q = "SELECT * FROM users WHERE email = ? OR username = ?";
 
-  db.query(q, [req.body.email, req.body.username], (err, data) => {
+  db.query(q, [req.body.email, req.body.username, req.body.imei], (err, data) => {
     if (err) return res.status(500).json(err);
     if (data.length) return res.status(409).json("User already exists!");
 
@@ -14,8 +14,8 @@ export const register = (req, res) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-    const values = [req.body.username, req.body.email, hash];
+    const q = "INSERT INTO users(`username`,`email`,`imei`,`password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, req.body.imei, hash];
 
     db.query(q, [values], (err, data) => {
       if (err) return res.status(500).json(err);
--- backend/controllers/download.js
+++ backend/controllers/download.js
@@ -8,21 +8,21 @@ const otps = {};
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'phuduccva@gmail.com', // Email của bạn
-        pass: '02803652498', // Mật khẩu email
+        user: '', // Email của bạn
+        pass: '', // Mật khẩu email
     },
 });
 
 export const sendOtp = (req, res) => {
-    const { username, email } = req.body;
+    const { imei } = req.body;
     // Kiểm tra đầu vào
-    if (!username && !email) {
+    if (!imei) {
         return res.status(400).json({ message: 'User ID is required.' });
     }
 
     // Lấy thông tin người dùng từ database
-    const q = 'SELECT email FROM users WHERE username = ? AND email = ?';
-    db.query(q, [username, email], async (err, results) => {
+    const q = 'SELECT imei FROM users WHERE imei = ?';
+    db.query(q, [imei], async (err, results) => {
         if (err || results.length === 0) {
             console.log(err);
             return res.status(404).json({ message: 'User not found.' });
@@ -32,10 +32,26 @@ export const sendOtp = (req, res) => {
 
         // Tạo OTP
         const otp = crypto.randomInt(100000, 999999).toString();
-        const otpExpires = Date.now() + 5 * 60 * 1000; // OTP hết hạn sau 5 phút
-
+        const otpExpires = Date.now() + 3 * 60 * 1000; // OTP hết hạn sau 3 phút
+        const updateQuery = 'UPDATE users SET otp = ? WHERE imei = ?';
+        db.query(updateQuery, [otp, imei], (updateErr) => {
+            if (updateErr) {
+                console.error(updateErr);
+                return res.status(500).json({ message: 'Failed to update OTP in database.' })
+            }
+            setTimeout(() => {
+                const deleteOtpQuery = 'UPDATE users SET otp = NULL WHERE imei = ?';
+                db.query(deleteOtpQuery, [imei], (deleteErr) => {
+                    if (deleteErr) {
+                        console.error(`Failed to delete OTP for IMEI ${imei}:`, deleteErr);
+                    } else {
+                        console.log(`OTP for IMEI ${imei} has been deleted.`);
+                    }
+                });
+            }, 3 * 60 * 1000);
+        })
         // Lưu OTP vào bộ nhớ
-        otps[email] = { otp, otpExpires, otpUsed: false };
+        otps[imei] = { otp, otpExpires, otpUsed: false };
 
         // Gửi OTP qua email
         try {
@@ -55,14 +71,14 @@ export const sendOtp = (req, res) => {
 }
 
 export const verifyOtp = (req, res) => {
-    const { email, otp } = req.body;
+    const { imei, otp } = req.body;
 
     // Kiểm tra đầu vào
-    if (!email || !otp) {
+    if (!imei || !otp) {
         return res.status(400).json({ message: 'User ID and OTP are required.' });
     }
 
-    const userOtpData = otps[email];
+    const userOtpData = otps[imei];
     if (!userOtpData) {
         return res.status(404).json({ message: 'No OTP found for this user.' });
     }
@@ -83,7 +99,17 @@ export const verifyOtp = (req, res) => {
     // Đánh dấu OTP đã sử dụng
     userOtpData.otpUsed = true;
 
-    res.status(200).json({ message: 'OTP verified successfully.' });
+    // res.status(200).json({ message: 'OTP verified successfully.' });
+    const deleteOtpQuery = 'UPDATE users SET otp = NULL WHERE imei = ?';
+    db.query(deleteOtpQuery, [imei], (deleteErr) => {
+        if (deleteErr) {
+            console.error(deleteErr);
+            return res.status(500).json({ message: 'Failed to delete OTP from database.' });
+        }
+
+        // Trả về kết quả thành công
+        res.status(200).json({ message: 'OTP verified and deleted successfully.' });
+    });
 }
 
 export const downloadFile = (req, res) => {
